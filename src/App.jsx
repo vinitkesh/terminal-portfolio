bad38c1f8fde3060867356dc32be3f93104e8064
Vinit Keshri <vinitkehri4171@gmail.com>
2024-12-15 06:23:48 +0530

    fix: deployment issue

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,10 @@ function App() {
   
 
   return (
+    <>
+    <div className='bg-img'></div>
     <div className="App max-h-screen min-h-screen overflow-clip flex items-center justify-center bg-[#A7AFD4] flex-col gap-0 ">
-      <div className='bg-img'></div>
+      
 
       <div className="w-full h-6 bg-black bg-opacity-25 absolute top-0">
         <div className="time origin-center left-[50%] absolute top-0 h-min">
@@ -29,6 +31,8 @@ function App() {
 
       
     </div>
+    
+    </>
   );
 }
 
